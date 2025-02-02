742aabea8fcf4e0155f9c182df3b735b937b5e6e
waleedmajeed001 <waleedmajeed630@gmail.com>
2025-02-02 10:17:10 +0500

    e-commerce

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -9,43 +10,42 @@ const Footer: React.FC = () => {
         <div className="container px-4 mx-auto">
           <div className="-mx-4 flex flex-wrap justify-between">
             <div className="px-4 my-4 w-full xl:w-1/5">
-              <a href="/" className="block w-56 mb-10">
+              <Link href="/" className="block w-56 mb-10">
                 <img
-                  src="/logofooter.png"  // Change this to the correct path to your image
+                  src="/logofooter.png"
                   alt="Tailwindow Logo"
-                  className="w-full"  // You can adjust the size as needed
+                  className="w-full"
                 />
-              </a>
+              </Link>
               <p className="text-justify">
-                Over compnay is new-oder now for copens.
+                Over company is new-order now for coupons.
               </p>
             </div>
 
             <div className="px-4 my-4 w-full sm:w-auto">
               <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Company</h2>
               <ul className="leading-8">
-                <li><a href="#" className="hover:text-blue-400">About Us</a></li>
-                <li><a href="#" className="hover:text-blue-400">Terms & Conditions</a></li>
-                <li><a href="#" className="hover:text-blue-400">Privacy Policy</a></li>
-                <li><a href="#" className="hover:text-blue-400">Contact Us</a></li>
+                <li><Link href="#" className="hover:text-blue-400">About Us</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">Terms & Conditions</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">Privacy Policy</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">Contact Us</Link></li>
               </ul>
             </div>
 
             <div className="px-4 my-4 w-full sm:w-auto">
               <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Blog</h2>
               <ul className="leading-8">
-                <li><a href="#" className="hover:text-blue-400 ">How To oder?</a></li>
-                <li><a href="#" className="hover:text-blue-400">How you get Whatsapp Number?</a></li>
-                <li><a href="#" className="hover:text-blue-400">How you can get mumbership?</a></li>
-                <li><a href="#" className="hover:text-blue-400">How to rate over app?</a></li>
-                <li><a href="#" className="hover:text-blue-400">Shopping Cart</a></li>
+                <li><Link href="#" className="hover:text-blue-400">How To order?</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">How you get Whatsapp Number?</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">How you can get membership?</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">How to rate our app?</Link></li>
+                <li><Link href="#" className="hover:text-blue-400">Shopping Cart</Link></li>
               </ul>
             </div>
 
             <div className="px-4 my-4 w-full sm:w-auto xl:w-1/5">
               <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Connect With Us</h2>
               <p className="hover:text-blue-400">0334 7905592</p>
-              {/* Add other social media links here */}
             </div>
           </div>
         </div>
